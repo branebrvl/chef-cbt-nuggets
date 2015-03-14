656aba9138c639b97f275a3d2beeb5677f01fa70
branislav <branebrvl@gmail.com>
2015-03-14 01:04:56 -0700

    php-pdo and php-msqli extension are installed

--- cookbooks/web_server/attributes/default.rb
+++ cookbooks/web_server/attributes/default.rb
@@ -1,4 +1,4 @@
-default['php']['packages'] = %w{ php php-common php-cli php-gd php-mysql php-pgsql php-sqlite php-pecl-memcached php-mcrypt php-fpm php-imagick php-intl php-xdebug}
+default['php']['packages'] = %w{ php php-common php-cli php-mbstring php-gd php-pdo php-mysqli php-mysql php-pgsql php-sqlite php-pecl-memcached php-mcrypt php-fpm php-imagick php-intl php-xdebug}
 
 default['yum']['remi-php55']['enabled'] = true
 
